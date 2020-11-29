3c7e10474dac730a83f4e3c1da380ba78c2c1de7
Stevö <stevehuwjohn@gmail.com>
2020-11-29 18:12:05 +0000

    Renaming

---
+++ src/Huffman.Tests/CompressionTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Huffman.Tests
+{
+    public class CompressionTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public CompressionTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Theory]
+        [InlineData("A Tale of Two Cities.txt")]
+        [InlineData("Frankenstein or the Modern Prometheus.txt")]
+        [InlineData("Great Expectations.txt")]
+        [InlineData("Les Misérables.txt")]
+        [InlineData("Pride and Prejudice.txt")]
+        [InlineData("War of the Worlds.txt")]
+        public void File_compresses_and_decompresses(string filename)
+        {
+            var file = File.ReadAllText($"Test Files\\{filename}");
+
+            _testOutputHelper.WriteLine($"Original size: {file.Length:N0} bytes.");
+
+            var timer = new Stopwatch();
+
+            timer.Start();
+
+            var compressed = Compression.Compress(file);
+
+            timer.Stop();
+
+            _testOutputHelper.WriteLine($"Compressed size: {compressed.Length:N0} bytes.");
+
+            _testOutputHelper.WriteLine($"Ratio: {(float) compressed.Length / file.Length *100:N2}%");
+            
+            _testOutputHelper.WriteLine($"Time taken to compress: {timer.ElapsedMilliseconds:N0} ms.");
+
+            timer.Reset();
+            timer.Start();
+
+            var decompressed = Compression.Decompress(compressed);
+
+            timer.Stop();
+
+            _testOutputHelper.WriteLine($"Decompressed size: {decompressed.Length:N0} bytes.");
+
+            _testOutputHelper.WriteLine($"Time taken to decompress: {timer.ElapsedMilliseconds:N0} ms.");
+
+            Assert.Equal(file, decompressed);
+        }
+    }
+}
---
+++ src/Huffman.Tests/Implementation/PriorityQueueTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Huffman.Implementation;
+using Xunit;
+
+namespace Huffman.Tests.Implementation
+{
+    public class PriorityQueueTests
+    {
+        [Fact]
+        public void PopMin_returns_items_in_the_correct_order()
+        {
+            var queue = new PriorityQueue<TestNode, int>();
+
+            queue.Add(new TestNode { Priority = 5, Description = "Five" });
+            queue.Add(new TestNode { Priority = 4, Description = "1-Four" });
+            queue.Add(new TestNode { Priority = 4, Description = "3-Four" });
+            queue.Add(new TestNode { Priority = 4, Description = "2-Four" });
+            queue.Add(new TestNode { Priority = 6, Description = "Six" });
+
+            Assert.Equal("1-Four", queue.PopMin().Description);
+            Assert.Equal("2-Four", queue.PopMin().Description);
+            Assert.Equal("3-Four", queue.PopMin().Description);
+            Assert.Equal("Five", queue.PopMin().Description);
+            Assert.Equal("Six", queue.PopMin().Description);
+        }
+
+        [Fact]
+        public void PopMin_throws_exception_when_unable_to_cast_priority_property()
+        {
+            var queue = new PriorityQueue<TestNode, bool>();
+
+            queue.Add(new TestNode { Priority = 5, Description = "Five" });
+
+            Assert.Throws<InvalidCastException>(() => queue.PopMin());
+        }
+    }
+
+    internal class TestNode
+    {
+        [Priority] 
+        public int Priority { get; set; }
+
+        [SecondarySort]
+        public string Description { get; set; }
+    }
+}

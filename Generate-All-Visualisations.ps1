08f60f69f0b8c02ea8b50243d181fe2e7a9111ca
Stevö <stevehuwjohn@gmail.com>
2020-12-02 13:25:24 +0000

    WIP

--- Generate-All-Visualisations.ps1
+++ Generate-All-Visualisations.ps1
@@ -1,3 +1,3 @@
-Set-Location .\Huffman.Console\bin\Debug\netcoreapp3.1
-Get-ChildItem "..\..\..\..\Huffman.Tests\Test Files\" | ForEach-Object { .\Huffman.Console.exe -f $_.FullName -o "..\..\..\..\visualisations\$($_.BaseName).html" }
-Set-Location ..\..\..\..\
+Set-Location .\src\Huffman.Console\bin\Debug\netcoreapp3.1
+Get-ChildItem "..\..\..\..\Huffman.Tests\Test Files\" | ForEach-Object { .\Huffman.Console.exe -f $_.FullName -o "..\..\..\..\..\visualisations\$($_.BaseName).html" }
+Set-Location ..\..\..\..\..\

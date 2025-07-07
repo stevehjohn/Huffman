d42e5fe9f2a6c77c81b20c8fe3d2b0227d433269
Stevo John <stevehuwjohn@gmail.com>
2025-07-07 17:59:11 +0100

    Tweak

--- benchmark-all.sh
+++ benchmark-all.sh
@@ -6,7 +6,7 @@ test_dir="./src/Huffman.Tests/Test Files"
 # Loop over all files in the directory
 for file in "$test_dir"/*; do
   echo ""
-  ./src/Huffman.Console/bin/Release/net6.0/Huffman.Console benchmark -f "$file"
+  ./src/Huffman.Console/bin/Release/net6.0/Huffman.Console benchmark -f "$file" $*
 done
 
 echo ""
